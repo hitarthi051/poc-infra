06662a3ba4015b1d515bea7267e42d938a73ef14
abc <abc@company.com>
2025-07-22 09:26:54 +0530

    commented api mapping

--- buildspec/update-route53-weighted.sh
+++ buildspec/update-route53-weighted.sh
@@ -5,17 +5,17 @@ DOMAIN="bg.infra.cpo-uk.icpo.altosaint.co.uk"
 REGION="eu-west-1"
 
 # Delete existing base path mappings
-for path in $(aws apigateway get-base-path-mappings --domain-name "$DOMAIN" --region "$REGION" --query 'items[*].basePath' --output text); do
-  aws apigateway delete-base-path-mapping --domain-name "$DOMAIN" --base-path "$path" --region "$REGION"
-done
+# for path in $(aws apigateway get-base-path-mappings --domain-name "$DOMAIN" --region "$REGION" --query 'items[*].basePath' --output text); do
+#   aws apigateway delete-base-path-mapping --domain-name "$DOMAIN" --base-path "$path" --region "$REGION"
+# done
 
-# Create new base path mapping (root)
-aws apigateway create-base-path-mapping \
-  --domain-name "$DOMAIN" \
-  --rest-api-id "$APIID" \
-  --stage "$STAGE" \
-  --base-path "(none)" \
-  --region "$REGION"
+# # Create new base path mapping (root)
+# aws apigateway create-base-path-mapping \
+#   --domain-name "$DOMAIN" \
+#   --rest-api-id "$APIID" \
+#   --stage "$STAGE" \
+#   --base-path "(none)" \
+#   --region "$REGION"
 
 echo "✅ Mapping updated: $DOMAIN -> $STAGE"
 
@@ -70,8 +70,11 @@ STABLE_ID=$(aws cloudfront list-distributions \
 
 aws cloudfront tag-resource \
   --resource "arn:aws:cloudfront::$ACCOUNT_ID:distribution/$CANARY_ID" \
-  --tags "Items=[{Key=TrafficType,Value=Stable},{Key=Deployment,Value=$STABLE_TAG}]"
+  --tags "Items=[{Key=TrafficType,Value=Stable}]"
 
+# ,{Key=Deployment,Value=$STABLE_TAG}
+# ,{Key=Deployment,Value=$CANARY_TAG}
 aws cloudfront tag-resource \
   --resource "arn:aws:cloudfront::$ACCOUNT_ID:distribution/$STABLE_ID" \
-  --tags "Items=[{Key=TrafficType,Value=Canary},{Key=Deployment,Value=$CANARY_TAG}]"
+  --tags "Items=[{Key=TrafficType,Value=Canary}]"
+  aws lambda add-permission --function-name "arn:aws:lambda:eu-west-1:057297422439:function:blue-green-api:green" --source-arn "arn:aws:execute-api:eu-west-1:057297422439:693utogn2j/*/GET/get-all-users" --principal apigateway.amazonaws.com --statement-id 78cb2b59-2666-4da5-9004-45d7c3c515fe --action lambda:InvokeFunction
